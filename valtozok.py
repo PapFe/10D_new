8b759d47717b9e74deb43542fb9e2887bec2cca1
Papfe <pap.martin227@gmail.com>
2022-09-22 09:41:41 +0200

    változók

---
+++ valtozok.py
@@ -0,0 +1,21 @@
+print("hello")
+
+szoveg_1 = "egyik"
+szoveg_2 = "másik"
+
+print(szoveg_1+szoveg_2)
+
+egesz_1 = 10
+egesz_2 = 6
+egesz_3 = egesz_1+egesz_2
+print(egesz_1/egesz_2)
+
+
+nemegesz_1 = 1.5
+nemegesz_2 = 20.168
+print(nemegesz_2)
+
+logika1 = True
+logika2 = False
+
+print(logika1 and logika2)
